c9d56e0d68b95d7bd8a3e6680f9504552fab77ce
overstep <c_h_e_n123@163.com>
2018-11-19 12:41:12 +0800

    sdqqq asdwed

---
+++ OG/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace OG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.步骤 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.分析栈 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.优先关系 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.当前符号 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.剩余符号 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.动作 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 44);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(708, 129);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(750, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "分析";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.Window;
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.步骤,
+            this.分析栈,
+            this.优先关系,
+            this.当前符号,
+            this.剩余符号,
+            this.动作});
+            this.listView1.ForeColor = System.Drawing.Color.Black;
+            this.listView1.Location = new System.Drawing.Point(9, 233);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(887, 293);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // 步骤
+            // 
+            this.步骤.Text = "步骤";
+            this.步骤.Width = 50;
+            // 
+            // 分析栈
+            // 
+            this.分析栈.Text = "分析栈";
+            this.分析栈.Width = 190;
+            // 
+            // 优先关系
+            // 
+            this.优先关系.Text = "优先关系";
+            this.优先关系.Width = 80;
+            // 
+            // 当前符号
+            // 
+            this.当前符号.Text = "当前符号";
+            this.当前符号.Width = 80;
+            // 
+            // 剩余符号
+            // 
+            this.剩余符号.Text = "剩余符号";
+            this.剩余符号.Width = 190;
+            // 
+            // 动作
+            // 
+            this.动作.Text = "动作";
+            this.动作.Width = 251;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "请在下面输入要分析的句子：";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(750, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 48);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "重置";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 530);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader 步骤;
+        private System.Windows.Forms.ColumnHeader 分析栈;
+        private System.Windows.Forms.ColumnHeader 优先关系;
+        private System.Windows.Forms.ColumnHeader 当前符号;
+        private System.Windows.Forms.ColumnHeader 剩余符号;
+        private System.Windows.Forms.ColumnHeader 动作;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ OG/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OG
+{
+    public partial class Form1 : Form
+    {
+        //优先关系矩阵
+        int[,] re={ 
+            { 1, -1, -1, -1, 1, 1},
+            { 1,  1, -1, -1, 1, 1},
+            { 1,  1,  2,  2, 1, 1},
+            { -1,-1, -1, -1, 0, 2},
+            { 1,  1,  2,  2, 1, 1},
+            { -1, -1,-1, -1, 2, 0}
+        };
+        String st;
+        int p;
+        int j;
+        int h;
+        char a;
+        String s;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void init(int n, String r)
+        {
+            st = "#";
+            p = 0;
+            s = r;
+            h = 0;
+        }
+        public int num(char c)
+        {
+            switch(c)
+            {
+                case '+':
+                    return 0;
+                case '*':
+                    return 1;
+                case 'i':
+                    return 2;
+                case '(':
+                    return 3;
+                case ')':
+                    return 4;
+                case '#':
+                    return 5;
+                default:
+                    return -1;
+            }
+        }
+        public int jude(String r,int i)
+        {
+            if (p == 1 && st[p] == '#' && (r[i]== '+' || r[i] == '*'))
+            {
+                return 0; 
+            }
+            if ((r[i] == '+'|| r[i] == '*')&&(r[i-1]=='+'||r[i-1]=='*'))
+            {
+                return 0;
+            }
+            if (r[i] == '#' && (r[i-1] == '+' || r[i-1] == '*'))
+            {
+                return 0;
+            }
+            return 1;
+        }
+        public void show(int r,char g,int d,int b)//r为优先关系值，g为当前符号，d=0表示移进，d=1表示规约；b=0表示分析失败，b=1表示分析继续，b=2表示分析成功
+        {
+            ListViewItem word = new ListViewItem();
+            word.Text = (++h).ToString();
+            word.SubItems.Add(st);
+                if (r == -1)
+                    word.SubItems.Add("<");
+                else if (r == 1) word.SubItems.Add(">");
+                else if(r==0) word.SubItems.Add("=");
+                else word.SubItems.Add("error");
+                word.SubItems.Add(g.ToString());
+                word.SubItems.Add(s);
+            if (b == 1)
+            {
+                if (d == 0)
+                    word.SubItems.Add("移进");
+                else word.SubItems.Add("规约");
+            }
+            else if(b==0)
+                word.SubItems.Add("分析失败！");
+            else if(b==2)
+                word.SubItems.Add("分析成功！");
+            listView1.Items.Add(word);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            int relation;
+            int i = 0;
+            String r = textBox1.Text;
+            if(r.Length<=0)
+            {
+                MessageBox.Show("input blank!");
+            }
+            else
+            {
+                init(r.Length, r);
+                while(1>0)
+                {
+                    a = r[i];
+                    if (jude(r,i)==0)
+                    {
+                        show(2, a, 1, 0);
+                        break;
+                    }
+                    if (num(st[p]) != -1) j = p;
+                    else j = p - 1;
+                    relation = re[num(st[j]), num(a)];
+                    if (relation == -1)
+                    {
+                        s = r.Substring(i + 1);
+                        show(relation, a, 0,1);
+                        p++;
+                        st += a.ToString();
+                        i++;
+                        continue;
+                    }
+                    else if(relation ==0)
+                    {
+                        if (st[j] == '#')
+                        {
+                            show(relation, a, 0, 2);
+                            break;
+                        }
+                        else
+                        {
+                            s = r.Substring(i + 1);
+                            show(relation, a, 0, 1);
+                            p++;
+                            st += a.ToString();
+                            i++;
+                            continue;
+                        }
+                    }
+                    else if(relation==1)
+                    {
+                        char q=st[j];
+                        if (num(st[j - 1])!=-1)
+                            j--;
+                        else
+                            j = j - 2;
+                        while (re[num(st[j]),num(q)]!=-1)
+                        {
+                            q = st[j];
+                            if (num(st[j - 1]) != -1)
+                                j--;
+                            else
+                                j = j - 2;
+                        }
+                        s = r.Substring(i + 1);
+                        show(relation, a, 1, 1);
+                        p = j + 1;
+                        if (st[p] == '(' || st[p] == 'i')
+                            st = st.Substring(0, p) + "F";
+                        else if (st[p+1] == '*')
+                            st = st.Substring(0, p) + "T";
+                        else st = st.Substring(0, p) + "E";
+                        continue;
+                    }
+                    else
+                    {
+                        show(2, a, 1, 0);
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+            listView1.Items.Clear();
+        }
+    }
+}
